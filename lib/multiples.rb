dbb19a172e129b2fca91dc5eeff5fb800fbcda99
rykyou <rkim1214@gmail.com>
2019-04-15 21:10:30 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,25 @@
-# Enter your procedural solution here!
+# Find the sum of all the multiples of 3 or 5 below 1000
+
+def collect_multiples(num)
+  i = 1
+  arr = []
+
+  while i < num
+    i % 3 == 0 || i % 5 == 0 ? arr.push(i) : 0
+    i += 1
+  end
+
+  arr
+end
+
+# puts collect_multiples(10)
+
+
+def sum_multiples(num)
+  sum = 0
+  collect_multiples(num).each { |a| sum+=a }
+  sum
+end
+
+
+puts sum_multiples(1000)
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,28 @@
-# Enter your object-oriented solution here!
+class Multiples
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+    i = 1
+    arr = []
+
+    while i < @limit
+      i % 3 == 0 || i % 5 == 0 ? arr.push(i) : nil
+      i += 1
+    end
+
+    arr
+  end
+
+  def sum_multiples
+    sum = 0
+    self.collect_multiples.each do |num|
+      sum += num
+    end
+    sum
+  end
+end
+
+# print Multiples.new(10).sum_multiples
+# print Multiples.new(10)
